7e58013c2b187b0c7297df5fb7decd41134fc42b
vladisa123 <47428311+vladisa123@users.noreply.github.com>
2021-07-29 15:38:26 +0300

    Добавлена фукция squares

--- first/nums.py
+++ first/nums.py
@@ -7,7 +7,13 @@ def minus(a,b):
 def mul(a,b):
     return a*b
 
+def squares ():
+    squares = [e*e for e in range(10) if e%2 == 0] # listcomps
+    return squares
+
+
 if __name__ == '__main__':
-    print(plus(1, 2))
-    print(minus(1, 2))
-    print(mul(1, 2))
+    # print(plus(1, 2))
+    # print(minus(1, 2))
+    # print(mul(1, 2))
+    print(squares())
